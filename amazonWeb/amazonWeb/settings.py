c4d75d562bf58cce477ff95e4aee678da65bc6fa
essieding <yuxin.ding18@gmail.com>
2023-04-28 14:27:27 -0400

    frontend debugged

--- amazonWeb/amazonWeb/settings.py
+++ amazonWeb/amazonWeb/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/amazonWeb/amazon/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'amazon/')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
